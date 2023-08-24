327bbd54d4f90b19503c638f7b149cf613171aa0
smoggy-P <smoggy@alumni.sjtu.edu.cn>
2023-08-24 15:43:22 +0200

    add visualization

--- src/metric_test_node.cpp
+++ src/metric_test_node.cpp
@@ -6,9 +6,12 @@
 #include <string.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <vector>
+#include <numeric>
+#include <visualization_msgs/Marker.h>
 using namespace std;
 
 ros::Subscriber sub;
+ros::Publisher pub, pub_pedestrain;
 ros::Time time0;
 bool msg_received = false;
 
@@ -60,39 +63,96 @@ void getParams(ros::NodeHandle n) {
 }
 
 void stateCB(const gazebo_msgs::ModelStatesConstPtr& msg) {
+  visualization_msgs::Marker marker_pedestrian;
+  marker_pedestrian.header.frame_id = "map";
+  marker_pedestrian.header.stamp = ros::Time();
+  marker_pedestrian.type = visualization_msgs::Marker::SPHERE_LIST;
+  geometry_msgs::Vector3 scale;
+  scale.x = 0.2;
+  scale.y = 0.2;
+  scale.z = 0.2;
+  marker_pedestrian.scale = scale;
+
+  for (auto itr=msg->name.begin(); itr!=msg->name.end(); itr++) {
+    if (isActor(*itr)) {
+      geometry_msgs::Point point;
+      point.x = msg->pose[itr - msg->name.begin()].position.x;
+      point.y = msg->pose[itr - msg->name.begin()].position.y;
+      point.z = 0;
+      marker_pedestrian.points.push_back(point);
+      std_msgs::ColorRGBA color;
+      color.r = 1;
+      color.g = 0;
+      color.b = 0;
+      color.a = 1;
+      marker_pedestrian.colors.push_back(color);
+    }
+  }
+  pub_pedestrain.publish(marker_pedestrian);
+
+  
   if (!msg_received) {
     msg_received = true;
     ROS_INFO("msg received");
     time0 = ros::Time::now();
   }
-  // ROS_INFO("drone_pos: %f, %f, %f at time %f", drone_pos.x, drone_pos.y, drone_pos.z, ros::Time::now().toSec() - time0.toSec());
-  // ROS_INFO("time: %f", time0.toSec());
-  if (ros::Time::now().toSec() - time0.toSec() > t_max) {
-    ROS_INFO("time up");
-    for (auto itr = time_survived_list.begin(); itr != time_survived_list.end(); itr++) {
-      ROS_INFO("time_survived: %f", *itr);
+  else{
+    if (ros::Time::now().toSec() - time0.toSec() > t_max) {
+      ROS_INFO("time up");
+      ROS_INFO("average time_survived: %f", std::accumulate(time_survived_list.begin(), time_survived_list.end(), 0.0) / time_survived_list.size());
+      ros::shutdown();
     }
-    ros::shutdown();
-  }
 
-  for (auto itr = msg->name.begin(); itr != msg->name.end(); itr++) {
-    if (isActor(*itr)) {
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time();
+    marker.type = visualization_msgs::Marker::SPHERE_LIST;
+    geometry_msgs::Vector3 scale;
+    scale.x = 1;
+    scale.y = 1;
+    scale.z = 1;
+    marker.scale = scale;
+    vector<std_msgs::ColorRGBA> colors;
+    for (auto itr = time_survived_list.begin(); itr != time_survived_list.end(); itr++) {
+      std_msgs::ColorRGBA color;
+      color.r = 0;
+      color.g = 0;
+      color.b = 1;
+      color.a = 1;
+      colors.push_back(color);
+    }
 
-      for (auto itr2 = drone_poses.begin(); itr2 != drone_poses.end(); itr2++) {
-        float x_robot = itr2->position.x;
-        float y_robot = itr2->position.y;
-        float x_actor = msg->pose[itr - msg->name.begin()].position.x;
-        float y_actor = msg->pose[itr - msg->name.begin()].position.y;
-        float dist = sqrt(pow(x_robot - x_actor, 2) + pow(y_robot - y_actor, 2));
-        if (dist < r_robot) {
-          float time_survived = ros::Time::now().toSec() - time0.toSec();
-          if (time_survived < time_survived_list[itr2 - drone_poses.begin()]) {
-            time_survived_list[itr2 - drone_poses.begin()] = time_survived;
+    for (auto itr = msg->name.begin(); itr != msg->name.end(); itr++) {
+      if (isActor(*itr)) {
+        for (auto itr2 = drone_poses.begin(); itr2 != drone_poses.end(); itr2++) {
+          float x_robot = itr2->position.x;
+          float y_robot = itr2->position.y;
+          float x_actor = msg->pose[itr - msg->name.begin()].position.x;
+          float y_actor = msg->pose[itr - msg->name.begin()].position.y;
+          float dist = sqrt(pow(x_robot - x_actor, 2) + pow(y_robot - y_actor, 2));
+          if (dist < r_robot) {
+            float time_survived = ros::Time::now().toSec() - time0.toSec();
+            if (time_survived < time_survived_list[itr2 - drone_poses.begin()]) {
+              time_survived_list[itr2 - drone_poses.begin()] = time_survived;
+            }
+            colors[itr2 - drone_poses.begin()].r = 1;
+            colors[itr2 - drone_poses.begin()].b = 0;
           }
         }
       }
     }
+    for (auto itr = drone_poses.begin(); itr != drone_poses.end(); itr++) {
+      geometry_msgs::Point point;
+      point.x = itr->position.x;
+      point.y = itr->position.y;
+      point.z = 0;
+      marker.points.push_back(point);
+      marker.colors.push_back(colors[itr - drone_poses.begin()]);
+    }
+    pub.publish(marker);
   }
+
+  
   // for (auto itr = time_survived_list.begin(); itr != time_survived_list.end(); itr++) {
   //     ROS_INFO("time_survived: %f", *itr);
   //   }
@@ -115,6 +175,9 @@ int main(int argc, char** argv) {
   }
 
   sub = n.subscribe("/gazebo/model_states", 100, stateCB);
+  pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+  pub_pedestrain = n.advertise<visualization_msgs::Marker>("pedestrain_marker", 10);
+
   ros::spin();
   return 0;
 }
